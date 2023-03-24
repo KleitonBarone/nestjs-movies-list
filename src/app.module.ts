b581dbb7395001f94103893573126a1ba7d674d5
Kleiton Barone <kleitonpolar6@gmail.com>
2023-03-24 20:26:46 -0300

    Added environment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+	EnvironmentConfigModule,
+	ConfigModule.forRoot({
+		isGlobal: true,
+		expandVariables: true,
+		envFilePath: '.env',
+	      }),
+],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/infrastructure/config/environment-config/environment-config.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentConfigService } from './environment-config.service';
+
+@Module({
+  providers: [EnvironmentConfigService]
+})
+export class EnvironmentConfigModule {}
